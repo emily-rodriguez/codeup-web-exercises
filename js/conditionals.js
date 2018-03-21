dba7e4644e80e01dbadb610392fd8e5c611bf5af
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-03-21 11:29:53 -0500

    added console.logs to help make clear when functions are being executed

--- js/conditionals.js
+++ js/conditionals.js
@@ -16,47 +16,49 @@
  * Can you refactor your code to use functions?
  */
 
+console.log("starting 'enter number exercise");
 
-// var enterNumber = confirm("Would you like to enter a number?");
-//
-// console.log(enterNumber);
-//
-// if (enterNumber) {
-//     var userNumber = prompt("Please enter a number");
-// } else {
-//     alert("You did not enter a number");
-// }
-//
-// var transformedNumber = parseFloat(userNumber);
-//
-// console.log(transformedNumber);
-//
-// var numberIsEven = (transformedNumber % 2) === 0;
-//
-// if (numberIsEven) {
-//     alert("The number you entered is even.");
-// } else {
-//     alert("The number you entered is odd.");
-// }
-//
-// function numberPlus100(transformedNumber) {
-//     return (transformedNumber + 100);
-// }
-//
-// alert("The number you entered + 100 = " + numberPlus100(transformedNumber));
-//
-//
-// var numberIsPositive = (transformedNumber > 0);
-// var numberIsNegative = (transformedNumber < 0);
-//
-// if (numberIsPositive) {
-//     alert("The number you entered is positive.");
-// } else if (numberIsNegative) {
-//     alert("The number you entered is negative.");
-// } else {
-//     alert("The number you entered is 0 or not a number");
-// }
+var enterNumber = confirm("Would you like to enter a number?");
 
+console.log(enterNumber);
+
+if (enterNumber) {
+    var userNumber = prompt("Please enter a number");
+} else {
+    alert("You did not enter a number");
+}
+
+var transformedNumber = parseFloat(userNumber);
+
+console.log(transformedNumber);
+
+var numberIsEven = (transformedNumber % 2) === 0;
+
+if (numberIsEven) {
+    alert("The number you entered is even.");
+} else {
+    alert("The number you entered is odd.");
+}
+
+function numberPlus100(transformedNumber) {
+    return (transformedNumber + 100);
+}
+
+alert("The number you entered + 100 = " + numberPlus100(transformedNumber));
+
+
+var numberIsPositive = (transformedNumber > 0);
+var numberIsNegative = (transformedNumber < 0);
+
+if (numberIsPositive) {
+    alert("The number you entered is positive.");
+} else if (numberIsNegative) {
+    alert("The number you entered is negative.");
+} else {
+    alert("The number you entered is 0 or not a number");
+}
+
+console.log("ending 'enter number exercise");
 
 
 /* ########################################################################## */
@@ -79,64 +81,66 @@
  * console.logging the function's return value
  */
 
-// var color = prompt("Please enter a color");
+console.log("starting 'enter a color' exercise");
+var color = 'red';
 
-// function analyzeColor(color) {
-//     if (color === "blue") {
-//         alert("blue is the color of the sky");
-//     } else if (color === "red"){
-//         alert("Strawberries are red");
-//     } else if (color === "cyan") {
-//         alert("I don't know anything about cyan");
-//     }
-// }
-//
-// analyzeColor("red");
+function analyzeColor(color) {
+    if (color === "blue") {
+        alert("blue is the color of the sky");
+    } else if (color === "red"){
+        alert("Strawberries are red");
+    } else if (color === "cyan") {
+        alert("I don't know anything about cyan");
+    }
+}
+
+analyzeColor("red");
 
 // Don't change the next two lines!
 // These lines create two variables for you:
 // - `colors`: a list of the colors of the rainbow
 // - `randomColor`: contains a single random color value from the list (this
 //                  will contain a different color everytime the page loads)
-// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-// var randomColor = colors[Math.floor(Math.random() * colors.length)];
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random() * colors.length)];
 /**
  * TODO:
  * Pass the `randomColor` variable to your function and console.log the results.
  * You should see a different message everytime you refresh the page
  */
-
-// console.log(randomColor);
-// console.log(analyzeColor(randomColor));
+console.log("selecting a random color");
+console.log(randomColor);
+console.log(analyzeColor(randomColor));
 
 /**
  * TODO:
  * Refactor your above function to use a switch-case statement
  */
 
-// switch(randomColor) {
-//     case "red":
-//         alert("Strawberries are red");
-//         break;
-//     case "orange":
-//         alert("Oranges are orange");
-//         break;
-//     case "yellow":
-//         alert("The sun is yellow");
-//         break;
-//     case "green":
-//         alert("The grass is green");
-//         break;
-//     case "blue":
-//         alert("The sky is blue");
-//         break;
-//     case "indigo":
-//         alert("Indigo girls?");
-//         break;
-//     case "violet":
-//         alert("You're turning violet, Violet");
-//         break;
-// }
+switch(randomColor) {
+    case "red":
+        alert("Strawberries are red");
+        break;
+    case "orange":
+        alert("Oranges are orange");
+        break;
+    case "yellow":
+        alert("The sun is yellow");
+        break;
+    case "green":
+        alert("The grass is green");
+        break;
+    case "blue":
+        alert("The sky is blue");
+        break;
+    case "indigo":
+        alert("Indigo girls?");
+        break;
+    case "violet":
+        alert("You're turning violet, Violet");
+        break;
+}
+
 
 
 /**
@@ -146,9 +150,12 @@
  * function to show it to the user.
  */
 
-// var userInputColor = prompt("Please enter a color");
-//
-// console.log(analyzeColor(userInputColor));
+var userInputColor = prompt("Please enter a color");
+
+console.log(analyzeColor(userInputColor));
+
+console.log("End of 'enter a color' exercise");
+
 
 /* ########################################################################## */
 
@@ -172,6 +179,7 @@
  * return value.
  */
 
+console.log("starting 'calculate total' exercise");
 
 function calculateTotal(x,y) {
     switch(luckyNumber) {
@@ -194,8 +202,6 @@ function calculateTotal(x,y) {
 return y - (x * y)
 }
 
-// console.log(calculateTotal(randomNumber,100));
-
 
 /**
  * TODO:
@@ -212,3 +218,5 @@ console.log("Your luckyNumber is " + luckyNumber);
 var billTotal = prompt("What was the total amount of your bill?");
 
 console.log("Your total, after the discount, comes to $" + parseFloat(calculateTotal(luckyNumber,billTotal)).toFixed(2));
+
+console.log("End of 'calculate total' exercise");
