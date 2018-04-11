0f183109ebc23ba2a2563ee1c466b1950f0d6ca4
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-04-11 14:37:56 -0500

    exercise: weather map exercise, working on updated weather data when marker is dragged

--- js/weather_js.js
+++ js/weather_js.js
@@ -1,34 +1,41 @@
 "use strict";
 
+var sanAntonio = {
+    lat: 29.4241,
+    lng: -98.4936
+};
 
 var forecast = $.get("http://api.openweathermap.org/data/2.5/forecast", {
     APPID: "71f25e5faa7c8d90ac72d5eae5ed34b3",
-    q:     "San Antonio, US",
+    q: "San Antonio",
     units: "imperial",
     cnt: 3
 });
 
-forecast.fail(function(jqXHR, status, error) {
-    console.log(status);
-    console.log(error);
-});
+(function getForecast() {
 
-forecast.done(function(response){
-    console.log(response);
+    forecast.fail(function (jqXHR, status, error) {
+        console.log(status);
+        console.log(error);
+    });
 
-    $('#weather-panels').html("");
+    forecast.done(function (response) {
+        console.log(response);
 
-    response.list.forEach(function(day){
-        $('#weather-panels').append(
-            "<div class='col card text-white text-center bg-primary'><div class='card-body'><h3>" + Math.round(day.main.temp_max) + "&deg/ " +
+        $('#weather-panels').html("");
+
+        response.list.forEach(function (day) {
+            $('#weather-panels').append(
+                "<div class='col card text-white text-center bg-primary'><div class='card-body'><h3>" + Math.round(day.main.temp_max) + "&deg/ " +
                 Math.round(day.main.temp_min) + "&deg" + "</h3>" +
                 "<h6>" + "<strong>" + day.weather[0].main + ": </strong>" + day.weather[0].description + "</h6>" +
                 "<h6>" + "<strong>Humidity: </strong>" + day.main.humidity + "</h6>" +
                 "<h6>" + "<strong>Wind: </strong>" + day.wind.speed + "</h6>" +
                 "<h6>" + "<strong>Pressure: </strong>" + day.main.pressure + "</h6>"
-        );
+            );
+        });
     });
-});
+})();
 
 var mapOptions = {
     zoom: 4,
@@ -37,9 +44,25 @@ var mapOptions = {
         lng: -98.5795
     }
 };
+
 var mapCanvas = document.getElementById('map-canvas');
 
 var map = new google.maps.Map(mapCanvas, mapOptions);
 
 var geocoder = new google.maps.Geocoder();
+
+
+var marker = new google.maps.Marker({
+    position: sanAntonio,
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP
+});
+
+google.maps.event.addListener(marker, 'dragend', function(event){
+    console.log(event.latLng.lat());
+    console.log(event.latLng.lng());
+    });
+
+
 
