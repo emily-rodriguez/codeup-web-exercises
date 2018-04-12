e2e384ca4883760933d925f9ef808067ac4f8011
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-04-12 09:53:19 -0500

    exercise: weather map exercise, complete

--- js/weather_js.js
+++ js/weather_js.js
@@ -5,14 +5,17 @@ var sanAntonio = {
     lng: -98.4936
 };
 
-var forecast = $.get("http://api.openweathermap.org/data/2.5/forecast", {
-    APPID: "71f25e5faa7c8d90ac72d5eae5ed34b3",
-    q: "San Antonio",
-    units: "imperial",
-    cnt: 3
-});
 
-(function getForecast() {
+
+function getForecast(lat, lon) {
+
+    var forecast = $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: "71f25e5faa7c8d90ac72d5eae5ed34b3",
+        lat: lat,
+        lon: lon,
+        units: "imperial",
+        cnt: 3
+    });
 
     forecast.fail(function (jqXHR, status, error) {
         console.log(status);
@@ -25,17 +28,38 @@ var forecast = $.get("http://api.openweathermap.org/data/2.5/forecast", {
         $('#weather-panels').html("");
 
         response.list.forEach(function (day) {
-            $('#weather-panels').append(
-                "<div class='col card text-white text-center bg-primary'><div class='card-body'><h3>" + Math.round(day.main.temp_max) + "&deg/ " +
-                Math.round(day.main.temp_min) + "&deg" + "</h3>" +
-                "<h6>" + "<strong>" + day.weather[0].main + ": </strong>" + day.weather[0].description + "</h6>" +
+            $('#weather-panels').append("<div class='col card text-white text-center bg-primary'><div class='card-body'><h3>"
+                + Math.round(day.main.temp_max) + "&deg/ " + Math.round(day.main.temp_min) + "&#8457" +
+                "</h3>" + "<h6>" + "<strong>" + day.weather[0].main + ": </strong>" + day.weather[0].description + "</h6>" +
                 "<h6>" + "<strong>Humidity: </strong>" + day.main.humidity + "</h6>" +
                 "<h6>" + "<strong>Wind: </strong>" + day.wind.speed + "</h6>" +
-                "<h6>" + "<strong>Pressure: </strong>" + day.main.pressure + "</h6>"
-            );
+                "<h6>" + "<strong>Pressure: </strong>" + day.main.pressure + "</h6>")
         });
+
+        $('#city-name').html("");
+
+        $('#city-name').append(response.city.name);
+
+        // degreeSystem = "";
+        //
+        // function defineDegreesSystem(input){
+        //     console.log(input)
+        //     if(input === "US"){
+        //         console.log("yay")
+        //     }
+        //     else {
+        //         return degreeSystem = "metric"
+        //     }
+        // };
+        //
+        // defineDegreesSystem(response.city.country);
+
     });
-})();
+};
+
+getForecast(sanAntonio.lat, sanAntonio.lng);
+
+
 
 var mapOptions = {
     zoom: 4,
@@ -49,8 +73,6 @@ var mapCanvas = document.getElementById('map-canvas');
 
 var map = new google.maps.Map(mapCanvas, mapOptions);
 
-var geocoder = new google.maps.Geocoder();
-
 
 var marker = new google.maps.Marker({
     position: sanAntonio,
@@ -60,9 +82,56 @@ var marker = new google.maps.Marker({
 });
 
 google.maps.event.addListener(marker, 'dragend', function(event){
-    console.log(event.latLng.lat());
-    console.log(event.latLng.lng());
-    });
+    var newLat = event.latLng.lat();
+    var newLong = event.latLng.lng();
+    getForecast(newLat, newLong);
+});
+
+
+$('#search').click(function(event){
+    var geocoder = new google.maps.Geocoder();
+
+    var cityInput = $('#city-search').val();
+    console.log(cityInput);
+
+    function callback(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            console.log(results);
+            map.setCenter(results[0].geometry.location);
+            var inputLat = results[0].geometry.location.lat;
+            var inputLng = results[0].geometry.location.lng;
+            getForecast(inputLat, inputLng);
+            function moveMarker (map, marker) {
+                marker.setPosition(results[0].geometry.location);
+                map.panTo(results[0].geometry.location);
+            };
+
+            moveMarker(map, marker);
+
+        } else {
+            console.log("Geocoding was not successful - STATUS: " + status);
+        }
+    }
+
+    geocoder.geocode({address: cityInput}, callback)
+
+
+});
+//
+//
+//         if (status == google.maps.GeocoderStatus.OK) {
+//             map.setCenter(results[0].geometry.location);
+//         } else {
+//             alert("Geocoding was not successful - STATUS: " + status);
+//         }
+//     });
+// });
+
+//have user input return lat/longs
+//run lat/longs through function
+
+
+
 
 
 
